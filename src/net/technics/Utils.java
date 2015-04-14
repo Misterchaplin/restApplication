98eac5d7639bcbbfeab15334b7895430d8be7a74
Misterchaplin <cbeaugrand@sts-sio-caen.info>
2015-04-14 18:13:06 +0200

    utilitaire

--- src/net/technics/Utils.java
+++ src/net/technics/Utils.java
@@ -4,10 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import net.controllers.AppController;
-import net.models.CollectionQuestionnaireGroupe;
 import net.models.Groupe;
 import net.models.Questionnaire;
-import net.models.Realisation;
 import net.models.Utilisateur;
 
 public class Utils {
@@ -18,16 +16,46 @@ public class Utils {
 		 Questionnaire[] lesQuestionnaires = Http.getAllQuestionnaires();
 		 
 		 if(lesQuestionnaires!=null && lesQuestionnaires.length>0){
-		        for(Questionnaire unQuestionnaire:lesQuestionnaires){
-		        	Realisation[] lesRealisations = Http.getQuestionnaireToRealisation(AppController.getActiveUser().getWho());     	 
-		        	for (Realisation uneRealisation : lesRealisations) {	
-		        		if(uneRealisation.getUtilisateur_id()==AppController.getActiveUser().getWho()){
-		        			myQestionnaires.add(unQuestionnaire);
-		        		}
+			 for(Questionnaire unQuestionnaire:lesQuestionnaires){
+		        Groupe[] lesGroupes = Http.getGroupesToQuestionnaire(unQuestionnaire.getId());     	 
+		        for (Groupe unGroupe : lesGroupes) {	
+		        	Utilisateur[] lesUsers = Http.getUtilisateursToGroupe(unGroupe.getId());
+		        	for (Utilisateur aUtilisateur : lesUsers) {
+			        	if(aUtilisateur.getId().equals(AppController.getActiveUser().getWho())){
+			        		if(!myQestionnaires.contains(unQuestionnaire)){
+			        			myQestionnaires.add(unQuestionnaire);
+			        		}
+			        		
+			        	}
 					}
 		        }
-	        }
+			 }
+		}
 		 Questionnaire[] tabQuestionnaie = myQestionnaires.toArray(new Questionnaire[myQestionnaires.size()]);
 		return tabQuestionnaie;
 	}
+	
+	
+	public static Groupe[] getGroupeToAUtilisateur(){
+		List<Groupe> myGroupes = new ArrayList<Groupe>();
+		
+		Groupe[] lesGroupes = Http.getAllGroupes();
+		 
+		 if(lesGroupes!=null && lesGroupes.length>0){
+			 for(Groupe unGroupe:lesGroupes){
+		        Utilisateur[] lesUsers = Http.getUtilisateursToGroupe(unGroupe.getId());
+		        for (Utilisateur aUtilisateur : lesUsers) {
+			        if(aUtilisateur.getId().equals(AppController.getActiveUser().getWho())){
+			        	if(!myGroupes.contains(unGroupe)){
+			        		myGroupes.add(unGroupe);
+			        	}
+			        		
+			        }
+				}
+		     }
+		}
+		Groupe[] tabGroupe = myGroupes.toArray(new Groupe[myGroupes.size()]);
+		return tabGroupe;
+	}
+		
 }
