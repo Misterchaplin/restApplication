10bc0468096597da3f12ecfcc79b81ccdaeb17c9
Misterchaplin <cbeaugrand@sts-sio-caen.info>
2015-04-16 16:04:29 +0200

    plus

--- src/net/controllers/QcmController.java
+++ src/net/controllers/QcmController.java
@@ -43,210 +43,9 @@ public class QcmController implements SelectionListener {
 		vAccueil.getBtnAjouterQcm().addSelectionListener(new SelectionAdapter() {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
-				checkQuestGroupe = beginInsert();
-				
-				if(checkQuestGroupe==true && (nbTrueAnswer==1 || nbTrueAnswer==2)){
-					if(session_id==null){
-						insertQuestionnaire = Http.postQuestionnarie(leQuestionnaire);
-						//Association entre groupe et questionnaire
-						GroupeQuestionnaire eventGroupeQuestionnaire = addOrUpdateGroupeQuestionnaire(insertQuestionnaire);
-						
-					}
-					if(session_id==null){
-						//Insertion d'une question appartenant à un questionnaire
-						laQuestion.setQuestionnaire_id(insertQuestionnaire.getId());
-					}else{
-						//Sinon on prend l'id du questionnaire précédent
-						laQuestion.setQuestionnaire_id(session_id.getId());
-					}
-					
-					Question insertQuestion=addOrUpdateQuestion();
-					//Insertion des reponses de la question
-					boolean insertCheckReponse=addOrUpdateReponse(insertQuestion);
-					
-					if(session_id==null){
-						//Si tout est correct alors on associe les précédents ajout à l'utilisateur (s'il la relation n'existe pas encore)
-						if(insertCheckReponse==true){				
-							boolean guCheck=ifGroupeWithUser();
-							boolean insertGrood=false;
-							if(guCheck==false){
-								GroupeUtilisateur insertGroupeUtilisateur = Http.postGroupeUtilisateurs(createGroupeUtilisateur());
-								insertGrood=true;
-								
-							}
-							else{
-								insertGrood=true;
-							}
-							
-							if(insertGrood==true){
-								endInsert();
-							}
-						}
-					}else{
-						endInsert();
-					}
-				}else{
-					vAccueil.getLblInformation().setText("Un ou plusieurs champs sont manquants");
-				}
-	
+				AddQcm();
 			}
 			
-			public boolean beginInsert(){
-				Integer stateAnswer=createReponse();
-				
-				if(stateAnswer!=0){
-					leQuestionnaire=createQuestionnaire();
-					leGroupe=createGroupe();
-					laQuestion=createQuestion();
-					if(leQuestionnaire.getLibelle().isEmpty() || laQuestion.getLibelle().isEmpty()){
-						checkQuestGroupe=false;
-					}
-					if(leGroupe.getId()==null){
-						checkQuestGroupe=false;
-					}
-				}
-				return checkQuestGroupe;
-			}
-			
-			public void endInsert(){
-				vAccueil.getBtnNouveauQuestionnaire().setVisible(true);
-				session_id=insertQuestionnaire;
-				System.out.println(session_id);
-				vAccueil.getTxtQuestionQcm().setText("");
-				vAccueil.getTxtQcm1().setText("");
-				vAccueil.getTxtQcm2().setText("");
-				vAccueil.getTxtQcm3().setText("");
-				vAccueil.getTxtQcm4().setText("");
-				vAccueil.getBtnCkGroupe1().setSelection(false);
-				vAccueil.getBtnCkGroupe2().setSelection(false);
-				vAccueil.getBtnCkGroupe3().setSelection(false);
-				vAccueil.getBtnCkGroupe4().setSelection(false);
-				vAccueil.getLblInformation().setText("Ajout réussie");
-				countQuestion++;
-				vAccueil.getLblCurrentQuestion().setText(String.valueOf(countQuestion));
-				vAccueil.getLblLastQuestion().setText(String.valueOf(countQuestion));
-				Utils.updateTableViewer();
-				vAccueil.getCbvQuestionnaireGroupe().setInput(null);
-				Utils.remplirComboQuestionnaire();
-			}
-			
-			public boolean ifGroupeWithUser(){
-				GroupeUtilisateur leGroupUser=createGroupeUtilisateur();
-				GroupeUtilisateur[] getGroupeUtilisateur = Http.getGroupeUtilisateur(leGroupUser.getUtilisateur_id());
-				boolean guCheck = false;
-				for (GroupeUtilisateur gu : getGroupeUtilisateur) {
-					if((leGroupUser.getGroupe_id()==gu.getGroupe_id()) && leGroupUser.getUtilisateur_id()==gu.getUtilisateur_id()){
-						guCheck=true;
-					}
-				}
-				
-				return guCheck;
-			}
-			
-			public GroupeUtilisateur createGroupeUtilisateur(){
-				GroupeUtilisateur groupeUtilisateur = new GroupeUtilisateur();
-				groupeUtilisateur.setGroupe_id(leGroupe.getId());
-				groupeUtilisateur.setUtilisateur_id(AppController.getActiveUser().getWho());
-				return groupeUtilisateur;
-			}
-			
-			public boolean addOrUpdateReponse(Question insertQuestion){
-				boolean insertCheckReponse=true;
-				for (Reponse reponse : lesReponses) {
-					reponse.setQuestion_id(insertQuestion.getId());
-					Reponse insertReponse = Http.postReponse(reponse);
-					if(insertReponse.getId().equals(null)){
-						insertCheckReponse=false;
-					}
-				}
-				return insertCheckReponse;
-			}
-			
-			public Question addOrUpdateQuestion(){
-				Question insertQuestion = Http.postQuestion(laQuestion);
-				return insertQuestion;
-			}
-			
-			public GroupeQuestionnaire addOrUpdateGroupeQuestionnaire(Questionnaire questionnaire){
-				GroupeQuestionnaire groupeQuestionnaire = new GroupeQuestionnaire();
-				groupeQuestionnaire.setGroupe_id(leGroupe.getId());
-				groupeQuestionnaire.setQuestionnaire_id(questionnaire.getId());
-				GroupeQuestionnaire insertGroupeQuestionnaire = Http.postGroupeQuestionnaires(groupeQuestionnaire);
-				if(insertGroupeQuestionnaire.equals(null)){
-					System.out.println("Erreur dans l'insertion des reponses");
-				}
-				return insertGroupeQuestionnaire;
-			}
-			
-			public Groupe createGroupe(){
-				IStructuredSelection selection = (IStructuredSelection) vAccueil.getCbvQcm().getSelection();
-		        Groupe element = (Groupe)selection.getFirstElement();
-				Groupe groupe = new Groupe();
-				if(selection.size()==1){
-					groupe.setId(element.getId());
-				}
-				return groupe;
-			}
-			
-			public Questionnaire createQuestionnaire(){
-				Questionnaire questionnaire = new Questionnaire();
-				questionnaire.setLibelle(vAccueil.getTxtQcm().getText());
-				DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-				Date date = new Date();
-				questionnaire.setDate(dateFormat.format(date));
-				
-				return questionnaire;
-			}
-			
-			public Question createQuestion(){
-				Question question = new Question();
-				question.setLibelle(vAccueil.getTxtQuestionQcm().getText());
-				return question;
-			}
-			
-			public int createReponse(){
-				Reponse reponse1 = new Reponse();
-				reponse1.setLibelle(vAccueil.getTxtQcm1().getText());
-				reponse1.setGood((vAccueil.getBtnCkGroupe1().getSelection()) ? 1 : 0);
-				Reponse reponse2 = new Reponse();
-				reponse2.setLibelle(vAccueil.getTxtQcm2().getText());
-				reponse2.setGood((vAccueil.getBtnCkGroupe2().getSelection()) ? 1 : 0);
-				Reponse reponse3 = new Reponse();
-				reponse3.setLibelle(vAccueil.getTxtQcm3().getText());
-				reponse3.setGood((vAccueil.getBtnCkGroupe3().getSelection()) ? 1 : 0);
-				Reponse reponse4 = new Reponse();
-				reponse4.setLibelle(vAccueil.getTxtQcm4().getText());
-				reponse4.setGood((vAccueil.getBtnCkGroupe4().getSelection()) ? 1 : 0);
-				
-				
-				lesReponses.add(reponse1);
-				lesReponses.add(reponse2);
-				lesReponses.add(reponse3);
-				lesReponses.add(reponse4);
-				
-				nbTrueAnswer=0;
-				boolean check=true;
-				if(reponse1.getLibelle().isEmpty() || reponse2.getLibelle().isEmpty() || 
-					reponse3.getLibelle().isEmpty() || reponse4.getLibelle().isEmpty()){
-					check=false;
-				}
-				else{
-					
-					if(reponse1.getGood()==1){
-						nbTrueAnswer++;
-					}
-					if(reponse2.getGood()==1){
-						nbTrueAnswer++;
-					}
-					if(reponse3.getGood()==1){
-						nbTrueAnswer++;
-					}
-					if(reponse4.getGood()==1){
-						nbTrueAnswer++;
-					}
-				}
-				return nbTrueAnswer;
-			}
 		
 			@Override
 			public void widgetDefaultSelected(SelectionEvent arg0) {
@@ -254,6 +53,233 @@ public class QcmController implements SelectionListener {
 
 			}
 		});		
+		
+		vAccueil.getBtnNouveauQuestionnaire().addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				initAddQcm();
+				AddQcm();
+			}
+		});
+	}
+	
+	public void initAddQcm(){
+		vAccueil.getTxtQcm().setEnabled(true);
+		vAccueil.getCbQcm().setEnabled(true);
+		vAccueil.getTxtQcm().setText("");
+		session_id=null;
+		countQuestion=0;
+		vAccueil.getLblCurrentQuestion().setText(String.valueOf(countQuestion));
+		vAccueil.getLblLastQuestion().setText(String.valueOf(countQuestion));
+	}
+	
+	public void AddQcm(){
+		checkQuestGroupe = beginInsert();
+		
+		if(checkQuestGroupe==true && (nbTrueAnswer==1 || nbTrueAnswer==2)){
+			if(session_id==null){
+				insertQuestionnaire = Http.postQuestionnarie(leQuestionnaire);
+				//Association entre groupe et questionnaire
+				GroupeQuestionnaire eventGroupeQuestionnaire = addOrUpdateGroupeQuestionnaire(insertQuestionnaire);
+				
+			}
+			if(session_id==null){
+				//Insertion d'une question appartenant à un questionnaire
+				laQuestion.setQuestionnaire_id(insertQuestionnaire.getId());
+			}else{
+				//Sinon on prend l'id du questionnaire précédent
+				laQuestion.setQuestionnaire_id(session_id.getId());
+			}
+			
+			Question insertQuestion=addOrUpdateQuestion();
+			//Insertion des reponses de la question
+			boolean insertCheckReponse=addOrUpdateReponse(insertQuestion);
+			
+			if(session_id==null){
+				//Si tout est correct alors on associe les précédents ajout à l'utilisateur (s'il la relation n'existe pas encore)
+				if(insertCheckReponse==true){				
+					boolean guCheck=ifGroupeWithUser();
+					boolean insertGrood=false;
+					if(guCheck==false){
+						GroupeUtilisateur insertGroupeUtilisateur = Http.postGroupeUtilisateurs(createGroupeUtilisateur());
+						insertGrood=true;
+						
+					}
+					else{
+						insertGrood=true;
+					}
+					
+					if(insertGrood==true){
+						endInsert();
+					}
+				}
+			}else{
+				endInsert();
+			}
+		}else{
+			vAccueil.getLblInformation().setText("Un ou plusieurs champs sont manquants");
+		}
+
+	}
+	
+	
+	public boolean beginInsert(){
+		Integer stateAnswer=createReponse();
+		
+		if(stateAnswer!=0){
+			leQuestionnaire=createQuestionnaire();
+			leGroupe=createGroupe();
+			laQuestion=createQuestion();
+			if(leQuestionnaire.getLibelle().isEmpty() || laQuestion.getLibelle().isEmpty()){
+				checkQuestGroupe=false;
+			}
+			if(leGroupe.getId()==null){
+				checkQuestGroupe=false;
+			}
+		}
+		return checkQuestGroupe;
+	}
+	
+	public void endInsert(){
+		vAccueil.getBtnNouveauQuestionnaire().setVisible(true);
+		session_id=insertQuestionnaire;
+		lesReponses.clear();
+		vAccueil.getTxtQuestionQcm().setText("");
+		vAccueil.getTxtQcm().setEnabled(false);
+		vAccueil.getCbQcm().setEnabled(false);
+		vAccueil.getTxtQcm1().setText("");
+		vAccueil.getTxtQcm2().setText("");
+		vAccueil.getTxtQcm3().setText("");
+		vAccueil.getTxtQcm4().setText("");
+		vAccueil.getBtnCkGroupe1().setSelection(false);
+		vAccueil.getBtnCkGroupe2().setSelection(false);
+		vAccueil.getBtnCkGroupe3().setSelection(false);
+		vAccueil.getBtnCkGroupe4().setSelection(false);
+		vAccueil.getLblInformation().setText("Ajout réussie");
+		countQuestion++;
+		vAccueil.getLblCurrentQuestion().setText(String.valueOf(countQuestion));
+		vAccueil.getLblLastQuestion().setText(String.valueOf(countQuestion));
+		Utils.updateTableViewer();
+		vAccueil.getCbvQuestionnaireGroupe().setInput(null);
+		Utils.remplirComboQuestionnaire();
+	}
+	
+	public boolean ifGroupeWithUser(){
+		GroupeUtilisateur leGroupUser=createGroupeUtilisateur();
+		GroupeUtilisateur[] getGroupeUtilisateur = Http.getGroupeUtilisateur(leGroupUser.getUtilisateur_id());
+		boolean guCheck = false;
+		for (GroupeUtilisateur gu : getGroupeUtilisateur) {
+			if((leGroupUser.getGroupe_id()==gu.getGroupe_id()) && leGroupUser.getUtilisateur_id()==gu.getUtilisateur_id()){
+				guCheck=true;
+			}
+		}
+		
+		return guCheck;
+	}
+	
+	public GroupeUtilisateur createGroupeUtilisateur(){
+		GroupeUtilisateur groupeUtilisateur = new GroupeUtilisateur();
+		groupeUtilisateur.setGroupe_id(leGroupe.getId());
+		groupeUtilisateur.setUtilisateur_id(AppController.getActiveUser().getWho());
+		return groupeUtilisateur;
+	}
+	
+	public boolean addOrUpdateReponse(Question insertQuestion){
+		boolean insertCheckReponse=true;
+		for (Reponse reponse : lesReponses) {
+			reponse.setQuestion_id(insertQuestion.getId());
+			Reponse insertReponse = Http.postReponse(reponse);
+			if(insertReponse.getId().equals(null)){
+				insertCheckReponse=false;
+			}
+		}
+		return insertCheckReponse;
+	}
+	
+	public Question addOrUpdateQuestion(){
+		Question insertQuestion = Http.postQuestion(laQuestion);
+		return insertQuestion;
+	}
+	
+	public GroupeQuestionnaire addOrUpdateGroupeQuestionnaire(Questionnaire questionnaire){
+		GroupeQuestionnaire groupeQuestionnaire = new GroupeQuestionnaire();
+		groupeQuestionnaire.setGroupe_id(leGroupe.getId());
+		groupeQuestionnaire.setQuestionnaire_id(questionnaire.getId());
+		GroupeQuestionnaire insertGroupeQuestionnaire = Http.postGroupeQuestionnaires(groupeQuestionnaire);
+		if(insertGroupeQuestionnaire.equals(null)){
+			System.out.println("Erreur dans l'insertion des reponses");
+		}
+		return insertGroupeQuestionnaire;
+	}
+	
+	public Groupe createGroupe(){
+		IStructuredSelection selection = (IStructuredSelection) vAccueil.getCbvQcm().getSelection();
+        Groupe element = (Groupe)selection.getFirstElement();
+		Groupe groupe = new Groupe();
+		if(selection.size()==1){
+			groupe.setId(element.getId());
+		}
+		return groupe;
+	}
+	
+	public Questionnaire createQuestionnaire(){
+		Questionnaire questionnaire = new Questionnaire();
+		questionnaire.setLibelle(vAccueil.getTxtQcm().getText());
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = new Date();
+		questionnaire.setDate(dateFormat.format(date));
+		
+		return questionnaire;
+	}
+	
+	public Question createQuestion(){
+		Question question = new Question();
+		question.setLibelle(vAccueil.getTxtQuestionQcm().getText());
+		return question;
+	}
+	
+	public int createReponse(){
+		Reponse reponse1 = new Reponse();
+		reponse1.setLibelle(vAccueil.getTxtQcm1().getText());
+		reponse1.setGood((vAccueil.getBtnCkGroupe1().getSelection()) ? 1 : 0);
+		Reponse reponse2 = new Reponse();
+		reponse2.setLibelle(vAccueil.getTxtQcm2().getText());
+		reponse2.setGood((vAccueil.getBtnCkGroupe2().getSelection()) ? 1 : 0);
+		Reponse reponse3 = new Reponse();
+		reponse3.setLibelle(vAccueil.getTxtQcm3().getText());
+		reponse3.setGood((vAccueil.getBtnCkGroupe3().getSelection()) ? 1 : 0);
+		Reponse reponse4 = new Reponse();
+		reponse4.setLibelle(vAccueil.getTxtQcm4().getText());
+		reponse4.setGood((vAccueil.getBtnCkGroupe4().getSelection()) ? 1 : 0);
+		
+		
+		lesReponses.add(reponse1);
+		lesReponses.add(reponse2);
+		lesReponses.add(reponse3);
+		lesReponses.add(reponse4);
+		
+		nbTrueAnswer=0;
+		boolean check=true;
+		if(reponse1.getLibelle().isEmpty() || reponse2.getLibelle().isEmpty() || 
+			reponse3.getLibelle().isEmpty() || reponse4.getLibelle().isEmpty()){
+			check=false;
+		}
+		else{
+			
+			if(reponse1.getGood()==1){
+				nbTrueAnswer++;
+			}
+			if(reponse2.getGood()==1){
+				nbTrueAnswer++;
+			}
+			if(reponse3.getGood()==1){
+				nbTrueAnswer++;
+			}
+			if(reponse4.getGood()==1){
+				nbTrueAnswer++;
+			}
+		}
+		return nbTrueAnswer;
 	}
 
 	@Override
